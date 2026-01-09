46d188e622a555e2fab9c2b63f8a5e5dcf2e5e16
hiro <hope_3722hk@yahoo.co.jp>
2026-01-09 15:18:09 +0900

    公開待ちステータスの追加と自動公開処理の実装

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,23 @@
-# Use this file to easily define all of your cron jobs.
-#
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + '/environment')
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-
+# cronを実行する環境変数周りの設定
+ENV.each { |k, v| env(k, v) }
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-# every 2.hours do
+every :hour do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
+rake 'article_state:update_article_state'
+end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
